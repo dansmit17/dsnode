77df635b745679367775171a785d81e7ea7cd50f
dansmit17 <dansmit@cisco.com>
2022-03-31 06:50:18 -0400

    Delete url-expansion directory

--- url-expansion/src/script.js
+++
@@ -1,8 +0,0 @@
-const target=document.getElementById('subId')
-
-target.addEventListener('paste', (event)=>{
-  setTimeout(function() {
-    window.open('https://ccrc.cisco.com/subscriptions/detail/'+event.target.value, '_blank')
-
-  }, 100);
-})
